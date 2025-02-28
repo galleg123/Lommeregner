c8de8e2d80e89662bd16e901173d3a8129fbcb08
Mikkel Silvasen <mikkelkp360@gmail.com>
2025-02-28 10:19:00 +0100

    Added comments to lommeregner.py

--- lommeregner.py
+++ lommeregner.py
@@ -1,21 +1,27 @@
 
 
-
+# Function to get number input from User
 def getNumberInput():
     num = input("\nInput a number:\n")
     return num
 
+
+
+#The main function that runs
 def performCalculation():
     
     currentValue = float(getNumberInput())
 
+
+    # Loop until specific input from user have been received
     while(True):
         operation = input("\nChoose operator by writing (+, -, *, /), if done write nothing:\n")
 
         if operation == "":
             break
 
-
+        
+        # Switch case on operation to perform different actions based on input
         match operation:
             case "+":
                 newNumber = float(getNumberInput())
@@ -31,6 +37,8 @@ def performCalculation():
                 currentValue /= newNumber
             case _:
                 print("Invalid input")
+
+
         print(f'\nCurrent Total is: {currentValue}')
     
     print(f'\nTotal equals: currentValue {currentValue}')
